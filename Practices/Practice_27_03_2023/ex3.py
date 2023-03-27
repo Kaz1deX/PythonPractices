2bbc38512624e7706b027a47055fbf74d74ee2de
Maxim Petrunin <max2003255@gmail.com>
2023-03-27 16:44:20 +0300

    Added Practice_27_03_2023

---
+++ Practices/Practice_27_03_2023/ex3.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import math
+
+
+def noise():
+    def noise(x, y):
+        sc_x = x * 12.9898
+        sc_y = y * 78.233
+        return (math.sin(sc_x + sc_y) * 1000)
+    arr = [noise(x, y) for x in range(50) for y in range(20)]
+    return arr
+
+
+noise_arr = noise()
+
+fig, ax = plt.subplots(figsize=(18, 6))
+ax.plot(noise_arr)
+plt.show()
