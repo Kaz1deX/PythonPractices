7ede962ede61b6af4e2ab9f313133d76fb995913
Kaz1deX <max2003255@gmail.com>
2023-04-17 00:12:32 +0300

    Add HomeWork_4

---
+++ Homeworks/HomeWork_4/ex3_1.py
@@ -0,0 +1,17 @@
+import graphviz
+
+
+def draw(vertices, edges):
+    dot = graphviz.Digraph()
+    # Вершины
+    for vertex in vertices:
+        dot.node(str(vertex[0]), vertex[1])
+    # Ребра
+    for edge in edges:
+        dot.edge(str(edge[0]), str(edge[1]))
+    # Граф
+    dot.format = 'png'
+    dot.render('C:\\Users\\Maxim\\PythonPractices\\Homeworks\\HomeWork_4\\graph', view=True)
+
+
+draw([(1, 'v1'), (2, 'v2')], [(1, 2), (2, 3), (2, 2)])
